b8b0954d3b1bcdd9304dd5f584b813aa5d4c1285
Efrain Hernandez <efrainhg2050@gmail.com>
2022-11-04 23:15:32 -0600

    Response for all methods in product controller

--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -29,17 +29,22 @@ public class ProductController {
     }
 
     @GetMapping("/category/{categoryId}")
-    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
-        return productService.getByCategory(categoryId);
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
+        return productService.getByCategory(categoryId).map(products-> new ResponseEntity<>(products, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/save")
-    public Product save(@RequestBody Product product) {
-        return productService.save(product);
+    public ResponseEntity<Product> save(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{productId}")
-    public boolean delete(@PathVariable("productId") int productId) {
-        return productService.delete(productId);
+    public ResponseEntity delete(@PathVariable("productId") int productId) {
+        if(productService.delete(productId)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
