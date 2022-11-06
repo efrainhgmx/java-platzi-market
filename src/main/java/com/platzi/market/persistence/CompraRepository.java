a24de9bfc64e22efbdf1b04601a2f4fc196045a0
Efrain Hernandez <efrainhg2050@gmail.com>
2022-11-05 18:30:06 -0600

    Repository method updated

--- src/main/java/com/platzi/market/persistence/CompraRepository.java
+++ src/main/java/com/platzi/market/persistence/CompraRepository.java
@@ -2,6 +2,10 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.domain.Purchase;
 import com.platzi.market.domain.repository.PurchaseRepository;
+import com.platzi.market.persistence.crud.CompraCrudRepository;
+import com.platzi.market.persistence.entity.Compra;
+import com.platzi.market.persistence.mapper.PurchaseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,15 +13,20 @@ import java.util.Optional;
 
 @Repository
 public class CompraRepository implements PurchaseRepository {
+    @Autowired
+    private CompraCrudRepository compraCrudRepository;
+    @Autowired
+    private PurchaseMapper mapper;
 
     @Override
     public List<Purchase> getAll() {
-        return null;
+        return mapper.toPurchases((List<Compra>) compraCrudRepository.findAll());
     }
 
     @Override
     public Optional<List<Purchase>> getByClient(String clientId) {
-        return Optional.empty();
+        return compraCrudRepository.findByIdCliente(clientId)
+                .map(compras -> mapper.toPurchases(compras));
     }
 
     @Override
