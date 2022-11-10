cd2cd8bf7b34cf65fd2021b1abd9b617371920ea
Efrain Hernandez <efrainhg2050@gmail.com>
2022-11-09 20:30:03 -0600

    Auth dtos

--- src/main/java/com/platzi/market/web/security/JWTUtil.java
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class JWTUtil {
     private static final String KEY = "platzi";
 
-    public String generateToke(UserDetails userDetails) {
+    public String generateToken(UserDetails userDetails) {
         return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, KEY).compact();
