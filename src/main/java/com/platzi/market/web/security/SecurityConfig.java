9db14f539e9380cfaed2bbec4da94a9b5f746333
Efrain Hernandez <efrainhg2050@gmail.com>
2022-11-09 20:50:40 -0600

    Enaable http reuquest in authenticate

--- src/main/java/com/platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.platzi.market.web.security;
 import com.platzi.market.domain.service.PlatziUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 @EnableWebSecurity
@@ -13,5 +14,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(platziUserDetailService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests().antMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated();
     }
 }
