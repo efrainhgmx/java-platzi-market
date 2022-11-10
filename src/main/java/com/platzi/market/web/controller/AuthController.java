34f15a7b6dc0ae1555aadedf55f01de4981b3148
Efrain Hernandez <efrainhg2050@gmail.com>
2022-11-09 20:40:21 -0600

    platzi details service and jwtutil

--- src/main/java/com/platzi/market/web/controller/AuthController.java
+++ src/main/java/com/platzi/market/web/controller/AuthController.java
@@ -2,10 +2,13 @@ package com.platzi.market.web.controller;
 
 import com.platzi.market.domain.dto.AuthenticationRequest;
 import com.platzi.market.domain.dto.AuthenticationResponse;
+import com.platzi.market.domain.service.PlatziUserDetailService;
+import com.platzi.market.web.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +21,17 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private PlatziUserDetailService platziUserDetailService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> createToken(@RequestBody AuthenticationRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-
+        UserDetails userDetails = platziUserDetailService.loadUserByUsername(request.getUsername());
+        String jwt = jwtUtil.generateToken(userDetails);
     }
 }
