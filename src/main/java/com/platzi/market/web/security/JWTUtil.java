8e043549ece3d47c3947a26ed00cb42f16a09dc6
Efrain Hernandez <efrainhg2050@gmail.com>
2022-11-14 19:52:32 -0600

    Validate token method

--- src/main/java/com/platzi/market/web/security/JWTUtil.java
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -18,13 +18,16 @@ public class JWTUtil {
     }
 
     public boolean validateToken(String token, UserDetails userDetails) {
-
+        return userDetails.getUsername().equals(extractUsername(token)) && !isTokenExpired(token);
     }
 
     public String extractUsername(String token) {
         return getClaims(token).getSubject();
     }
 
+    public boolean isTokenExpired(String token){
+        return getClaims(token).getExpiration().before(new Date());
+    }
     private Claims getClaims(String token) {
         return Jwts.parser().setSigningKey(KEY).parseClaimsJws(token).getBody();
     }
